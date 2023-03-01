b9a04500e7f1b580533dae9c8644b2bd88737b54
Dan Stronge <dnlstronge@gmail.com>
2023-03-01 10:24:00 +0000

    [x] - add phrase as param set type

--- app.ts
+++ app.ts
@@ -1,16 +1,18 @@
-function add(n1: number, n2: number, showresult: boolean) {
+
+const storedF = function add(n1: number, n2: number, showresult: boolean, phrase: string) {
   // if(typeof(n1) !== "number" || typeof(n2) !== "number") {
   //     throw new Error("Incorrect input!")
   // }
   if (showresult) {
-    console.log(n1 + n2);
+    console.log(`${phrase} ${n1 + n2} `);
   } else {
     return n1 + n2;
   }
 }
 
-const num1 = 5;
-const num2 = 7.4;
-const printResult = true;
+const n_1 = 5;
+const n_2 = 7.4;
+const show = true;
+const result_phrase = "And the result is: "
 
-add(num1, num2, printResult)
+storedF(n_1, n_2, show, result_phrase)
