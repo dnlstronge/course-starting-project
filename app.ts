c333454e54410f74aaa7b8eb7c5223dab3e3af17
Dan Stronge <dnlstronge@gmail.com>
2023-03-01 11:36:19 +0000

    [x] - add array [x] - add comment

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ const person: object = {
   age: 37,
 };
 
-console.log(person.nickname);
+console.log(person.name);
 
 /*
 JS objects have key value pairs:  {
@@ -33,12 +33,13 @@ const individual: { name: string; age: number } = {
   age: 44,
 };
 
-/* I get it, its far more explicit in what the object is. but...it can all be inferred!!! so this is fine */
+/* I get it, its far more explicit in what the object is. but...it can all be inferred!!! so this is fine:::> */
 
 const individual2 = {
     name: "BigDave", // infers string
-    age: 44 // infers number
-
+    age: 44, // infers number
+    hobbies: ["football", "boxing", "music", 25]
 }
 
-/* Ahhh.... */
+/* Ahhh....so if you add an array of strings => string[] */
+/* or an array of strings and a number like above ==> string||number[] */
