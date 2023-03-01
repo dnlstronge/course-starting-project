f05c17e642ed316481ec21d8490c8d7f88f9c2d1
Dan Stronge <dnlstronge@gmail.com>
2023-03-01 11:59:05 +0000

    [x] - add comments

--- app.ts
+++ app.ts
@@ -9,3 +9,12 @@ for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase())
 }
 
+// try something that should throw an error: 
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.map())
+}
+
+// map is only available on arrays so this should give an error
+
+/* error: Property 'map' does not exist on type 'string'. */
