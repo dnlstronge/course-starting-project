023214128fe9f755a7e3fbec180497bd159b0a80
Dan Stronge <dnlstronge@gmail.com>
2023-03-01 11:11:22 +0000

    [x] - add comments

--- app.ts
+++ app.ts
@@ -3,3 +3,27 @@ const person = {
     age: 37
 
 }
+
+console.log(person.nickname)
+
+/*
+JS objects have key value pairs:  {
+    key: value
+}
+TS object types have key: type pairs so; 
+
+const jsObject = {
+    name: "Benson",
+    id: 12345
+
+}
+if you hover over an object in IDE it shows this => 
+
+
+const jsObject: {
+    name: string;
+    id: number;
+}
+
+there are subtle differences
+*/
