21c2bef9743dcdb8ab84dcbde40bde902ff67ea9
Dan Stronge <dnlstronge@gmail.com>
2023-03-01 13:04:17 +0000

    [x] - add enum

--- app.ts
+++ app.ts
@@ -1,20 +1,32 @@
-const person: {
-    name: string;
-    age: number; 
-    hobbies: string[];
-    role: [number, string]
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
 
-} = {
-    name: "dan",
-    age: 37,
-    hobbies: ["football", "cooking"],
-    role: [1, "Surf"] // this is a TUPLE (array of fixed length and type but needs explicitly set as such above)
+// } = {
+//     name: "dan",
+//     age: 37,
+//     hobbies: ["football", "cooking"],
+//     role: [1, "Surf"] // this is a TUPLE (array of fixed length and type but needs explicitly set as such above)
 
-}
+// }
+const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2;
 
-person.role.push("running"); // push is an exemption and will work
-person.role[1] = 5 
+const person = {
+  name: "dan",
+  age: 37,
+  hobbies: ["football", "cooking"],
+  role: ADMIN,
+};
+
+//person.role.push("running");
+//person.role[1] = 5
 
 // this is allowed as the type is an array w/ either number or string
 
 // if you explicity set the type as a tuple TS should throw errors, try.....
+
+/* Typscript allows enums =====> enum {NEW, OLD}  */
