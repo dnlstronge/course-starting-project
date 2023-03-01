edb96772acd307b4c7ba5e2b10a4b0c24ad4447c
Dan Stronge <dnlstronge@gmail.com>
2023-03-01 12:10:02 +0000

    [x] - cause type error

--- app.ts
+++ app.ts
@@ -1,8 +1,14 @@
-const person = {
+const person: {
+    name: string;
+    age: number; 
+    hobbies: string[];
+    role: [number, string]
+
+} = {
     name: "dan",
     age: 37,
     hobbies: ["football", "cooking"],
-    role: [1, "Surf"] // this is a TUPLE (array of fixed length and type)
+    role: [1, "Surf"] // this is a TUPLE (array of fixed length and type but needs explicitly set as such above)
 
 }
 
@@ -11,4 +17,4 @@ person.role[1] = 5
 
 // this is allowed as the type is an array w/ either number or string
 
-
+// if you explicity set the type as a tuple TS should throw errors, try.....
