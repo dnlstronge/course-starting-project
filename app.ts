0a2705788131bc1057cf859359a73468557e0b3d
Dan Stronge <dnlstronge@gmail.com>
2023-03-02 09:04:29 +0000

    [x] add logic to if check

--- app.ts
+++ app.ts
@@ -3,8 +3,8 @@ function combine(
   input2: number | string, 
   converted: string) {
   let result: number | string;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (typeof input1 === "number" && typeof input2 === "number" || converted === "as_number") {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
