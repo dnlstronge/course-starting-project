93cc19f31a595898fc7024361ab2ec1ea319a4f6
Dan Stronge <dnlstronge@gmail.com>
2023-03-02 13:46:21 +0000

    [x] - check in browser expect 111 to be printed (clear)

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
-function Add( n1: number, n2: number): number {
-    return n1 + n2
-};
+function Add(n1: number, n2: number): number {
+  return n1 + n2;
+}
 
 function printNum(num: number) {
-    console.log("the result is " +num)
+  console.log("the result is " + num);
 }
+
+printNum(Add(55, 56));
