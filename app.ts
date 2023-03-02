fcee357e8cf6467f76090314e0d5764d51da48dc
Dan Stronge <dnlstronge@gmail.com>
2023-03-02 08:51:32 +0000

    [x] - use literal to output different things

--- app.ts
+++ app.ts
@@ -1,14 +1,25 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string, 
+  input2: number | string, 
+  converted: string) {
   let result: number | string;
   if (typeof input1 === "number" && typeof input2 === "number") {
     result = input1 + input2;
   } else {
     result = input1.toString() + input2.toString();
   }
+  if(converted === "as_number") {
+    result = +input1 + +input2
+  }
+  if(converted === "as_string") {
+    result = input1.toString() + input2.toString()
+  }
   return result;
 }
 
-const combinedStrings = combine("meagles", "ducks");
+const combinedStrings = combine(2, 5, "as_number");
 console.log(combinedStrings);
-const combinedNums = combine( 2, 3)
+const combinedboth = combine("2", "2", "as_number");
+console.log(combinedStrings);
+const combinedNums = combine( 2, 3, "as_string")
 console.log(combinedNums)
