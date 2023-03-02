13e0b544bc0e3b7d158172d60b0e08d938f8cc7c
Dan Stronge <dnlstronge@gmail.com>
2023-03-02 13:27:44 +0000

    [x] - exp new function

--- app.ts
+++ app.ts
@@ -1,3 +1,7 @@
-function Add( n1: number, n2: number) {
-    return
+function Add( n1: number, n2: number): number {
+    return n1 + n2
 };
+
+function printNum(num: number) {
+    console.log("the result is " +num)
+}
